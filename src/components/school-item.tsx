2c40892edec297f63862c8989424fb2ca53a0fea
nwobuaka <nwobuaka@gmail.com>
2025-01-09 19:15:01 +0000

    Changes school item to table row, added sorting to school list, changed represntation to a table

--- src/components/school-item.tsx
+++ src/components/school-item.tsx
@@ -9,5 +9,11 @@ type SchoolItemProps = {
 export const SchoolItem = ({
     school
 }: SchoolItemProps) => {
-    return <li className='school-item'>{school.name}</li>
+    return (
+        <tr>
+            <td>{school.name}</td>
+            <td>{school.average_student_gpa}</td>
+        </tr>
+    )
+     
 }
