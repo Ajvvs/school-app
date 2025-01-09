6666bf63b5e21cadeb26dc79e1c42c430d5aacbd
nwobuaka <nwobuaka@gmail.com>
2025-01-09 15:57:24 +0000

    New commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+
+export const App = ({ naame }) => {
+    // const [counters, setCounters] = useState<number[]>
+    
+    // return <h1 onClick={()=> {
+    //     setCounters(counters[] + 1, ..counters)
+    // }}>Hello World! Counter: {counters.join(',')}</h1>
+}
