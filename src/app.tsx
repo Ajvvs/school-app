d6a233c50afbe51a7ea27e8710004838ab5d51c7
nwobuaka <nwobuaka@gmail.com>
2025-01-09 16:43:40 +0000

    Inital feature school list Added a school list component, added a school item componnet, integrated then together and in the app. Also added a function to fetch school data, and used it in an effect

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react"
+import { SchoolList } from "./components/school-list"
 
-export const App = ({ naame }) => {
+export const App = () => {
     // const [counters, setCounters] = useState<number[]>
     
     // return <h1 onClick={()=> {
     //     setCounters(counters[] + 1, ..counters)
     // }}>Hello World! Counter: {counters.join(',')}</h1>
+
+    return <SchoolList/>
 }
